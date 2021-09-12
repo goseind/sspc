d2984be4e3ae5556f7e5a998a1c4fc88eb7e0497
otiofrui <spam@dev.otiofrui.org>
2021-09-12 08:30:56 -0700

    adding form

--- forms.py
+++ forms.py
@@ -1,11 +1,18 @@
 from flask_wtf import FlaskForm
 from wtfforms import DecimalField, StringField, SubmitField
+from wtfforms.validators import DataRequired
 
 class SavingsPlanCalculator(FlaskForm):
-    new_capital = DecimalField('Available Capital')
-    current_allocation_a = DecimalField('Asset Class A')
-    current_allocation_b = DecimalField('Asset Class B')
+    # General Fields (required)
+    new_capital = DecimalField('Available Capital', validators=[DataRequired()])
+    time_period = DecimalField('Time Period in Months', validators=[DataRequired()])
+
+    # Asset Fields (min. two required)
+    current_allocation_a = DecimalField('Asset Class A', validators=[DataRequired()])
+    current_allocation_b = DecimalField('Asset Class B', validators=[DataRequired()])
     current_allocation_c = DecimalField('Asset Class C')
     current_allocation_d = DecimalField('Asset Class D')
     current_allocation_e = DecimalField('Asset Class E')
+    
+    # Functional Fields
     calculate = SubmitField('Calculte new rates')
--- home.py
+++ home.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 # Routing for root directory
 @app.route("/")
 def home():
-    return render_template('savingsplan_calc.html')
+    form = SavingsPlanCalculator()
+    return render_template('savingsplan_calc.html', title='Calculator', form=form)
 
 # Makes it possible to call the file directly with pyhton instead of using the debug mode
 if __name__ == '__main__':
